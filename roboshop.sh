67fd6c765361d7115afee168975d855e564e942f
pennabadiumamaheswarreddy <pennabadiumamaheswarreddy@gmail.com>
2025-09-26 23:55:03 +0530

    variables

--- roboshop.sh
+++ roboshop.sh
@@ -9,9 +9,9 @@ do
 
     #Get Private IP
     if [ $instance !=  "frontend"]; then 
-        IP=$(aws ec2 describe-instances --instance-ids i-038828595447cfccc --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else 
-        IP=$(aws ec2 describe-instances --instance-ids i-038828595447cfccc --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
 
     echo "$instance: $IP"
